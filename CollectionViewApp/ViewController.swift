72e6b20507f21bc76ff5d60dfceb47c7a0de39d2
Bhumi Patel <bhumiv1999@yahoo.com>
2020-07-04 15:46:16 -0700

    Initial Commit

---
+++ CollectionViewApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewApp
+//
+//  Created by Bhumi Patel on 7/4/20.
+//  Copyright © 2020 Bhumi Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
